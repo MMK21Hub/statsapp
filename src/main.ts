21243d75c86d8a1838df435433a4af7a92299731
MMK21Hub <50421330+MMK21Hub@users.noreply.github.com>
2023-05-29 15:51:43 +0100

    Add messages per person

--- src/main.ts
+++ src/main.ts
@@ -5,22 +5,49 @@ const chatExportParser =
 
 const data = await readFile(process.argv[2], "utf8")
 
-const messagesPerDay = new Map<string, number>()
+const results: Record<string, Record<string, number>> = {}
 
 let match: RegExpMatchArray | null = chatExportParser.exec(data)
 while ((match = chatExportParser.exec(data))) {
   const [_, dateString, timeString, name, content] = match!
   const [day, month, year] = dateString.split("/").map((num) => parseInt(num))
-  const date = new Date(year, month, day)
+  const fullDate = new Date(year, month - 1, day)
 
-  const key = date.toString()
-  const oldCount = messagesPerDay.get(key) || 0
-  messagesPerDay.set(key, oldCount + 1)
+  const dateKey = fullDate.toLocaleDateString()
+  const nameKey = name.split(" ")[0]
+  if (!results[dateKey]) results[dateKey] = {}
+  const targetCount = results[dateKey][nameKey]
+  if (!targetCount) results[dateKey][nameKey] = 0
+  results[dateKey][nameKey]++
 }
 
-// console.log(messagesPerDay)
-messagesPerDay.forEach((count, dateString) => {
-  const date = new Date(dateString)
-  console.log(`${date.getDay}, ${count}`)
-})
+console.log(objectToCSV(results))
 debugger
+
+function objectToCSV(object: Record<string, Record<string, unknown>>) {
+  const dataAs2DArray: string[][] = [[""]]
+  const indexes = new Map<string, number>()
+
+  Object.entries(object).forEach(([rowHeading, rowData]) => {
+    const row = [rowHeading]
+    Object.entries(rowData).forEach(([colHeading, cell]) => {
+      const matchedIndex = indexes.get(colHeading)
+      if (matchedIndex) return (row[matchedIndex] = `${cell}`)
+
+      // We haven't encountered this key before, so make a new column
+      const lastIndex = Array.from(indexes.values()).at(-1) || 0
+      const newIndex = lastIndex + 1
+      indexes.set(colHeading, newIndex)
+      row[newIndex] = `${cell}`
+    })
+    dataAs2DArray.push(row)
+  })
+
+  // Add the headings for each column
+  indexes.forEach((index, label) => {
+    dataAs2DArray[0][index] = label
+  })
+
+  const dataAsCSV = dataAs2DArray.map((row) => row.join(",")).join("\n")
+  return dataAsCSV
+}
