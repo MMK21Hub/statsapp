d26dfe65f1987bcc745466063fa12fc1c228c18e
MMK21Hub <50421330+MMK21Hub@users.noreply.github.com>
2023-05-29 15:07:30 +0100

    Basic stat generation

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,26 @@
-import {} from "node:fs"
+import { readFile } from "node:fs/promises"
 
 const chatExportParser =
-  /^(\d{2}\/\d{2}\/\d{4}), (\d{1,2}:\d{2} [ap]m) - (.*): (.*)/
+  /^(\d{2}\/\d{2}\/\d{4}), (\d{1,2}:\d{2} [ap]m) - (.*): (.*)/gm
+
+const data = await readFile(process.argv[2], "utf8")
+
+const messagesPerDay = new Map<string, number>()
+
+let match: RegExpMatchArray | null = chatExportParser.exec(data)
+while ((match = chatExportParser.exec(data))) {
+  const [_, dateString, timeString, name, content] = match!
+  const [day, month, year] = dateString.split("/").map((num) => parseInt(num))
+  const date = new Date(year, month, day)
+
+  const key = date.toString()
+  const oldCount = messagesPerDay.get(key) || 0
+  messagesPerDay.set(key, oldCount + 1)
+}
+
+// console.log(messagesPerDay)
+messagesPerDay.forEach((count, dateString) => {
+  const date = new Date(dateString)
+  console.log(`${date.getDay}, ${count}`)
+})
+debugger
