d6bcdbc4cf87462c5a823c301e6eef5fcf60490a
MMK21Hub <50421330+MMK21Hub@users.noreply.github.com>
2023-05-29 18:44:37 +0100

    tyt to make a time card

--- src/main.ts
+++ src/main.ts
@@ -5,36 +5,73 @@ const chatExportParser =
 
 const data = await readFile(process.argv[2], "utf8")
 
-const results: Record<string, Record<string, number>> = {}
+const dailyStats: Record<string, Record<string, number>> = {}
+const hourlyStats: {
+  weekday: string
+  hour: string
+  count: number
+}[] = []
 
 let match: RegExpMatchArray | null = chatExportParser.exec(data)
 while ((match = chatExportParser.exec(data))) {
   const [_, dateString, timeString, name, content] = match!
   const [day, month, year] = dateString.split("/").map((num) => parseInt(num))
-  const fullDate = new Date(year, month - 1, day)
+  const date = new Date(year, month - 1, day)
 
-  const dateKey = fullDate.toLocaleDateString()
-  const nameKey = name.split(" ")[0]
-  if (!results[dateKey]) results[dateKey] = {}
-  const targetCount = results[dateKey][nameKey]
-  if (!targetCount) results[dateKey][nameKey] = 0
-  results[dateKey][nameKey]++
+  const timeParts = timeString.split(":").map((num) => parseInt(num))
+  const isAfternoon = timeString.includes("pm")
+  const hour = isAfternoon ? timeParts[0] + 12 : timeParts[0]
+  const minute = timeParts[1]
+  const dateTime = new Date(year, month - 1, day, hour, minute)
+
+  // Daily stats
+  {
+    const dateKey = date.toLocaleDateString()
+    const nameKey = name.split(" ")[0]
+    if (!dailyStats[dateKey]) dailyStats[dateKey] = {}
+    const currentCount = dailyStats[dateKey][nameKey]
+    if (!currentCount) dailyStats[dateKey][nameKey] = 0
+    dailyStats[dateKey][nameKey]++
+  }
+
+  // Hourly stats
+  {
+    const currentWeekday = toWeekday(date.getDay())
+    const currentHour = dateTime.getHours().toString()
+    const matchedIndex = hourlyStats.findIndex(
+      (point) => point.hour === currentHour && point.weekday === currentWeekday
+    )
+
+    if (matchedIndex === -1) {
+      hourlyStats.push({
+        count: 1,
+        hour: currentHour,
+        weekday: currentWeekday,
+      })
+    } else {
+      hourlyStats[matchedIndex].count++
+    }
+  }
 }
 
-console.log(objectToCSV(results))
+console.log(objectToCSV(hourlyStats))
 debugger
 
-function objectToCSV(object: Record<string, Record<string, unknown>>) {
+function objectToCSV(
+  object: Record<string, Record<string, unknown>> | Record<string, unknown>[]
+) {
+  const inputIsArray = Array.isArray(object)
   const dataAs2DArray: string[][] = [[""]]
   const indexes = new Map<string, number>()
 
   Object.entries(object).forEach(([rowHeading, rowData]) => {
-    const row = [rowHeading]
+    const row = inputIsArray ? [] : [rowHeading]
     Object.entries(rowData).forEach(([colHeading, cell]) => {
       const matchedIndex = indexes.get(colHeading)
       if (matchedIndex) return (row[matchedIndex] = `${cell}`)
 
       // We haven't encountered this key before, so make a new column
+      const startingIndex = inputIsArray
       const lastIndex = Array.from(indexes.values()).at(-1) || 0
       const newIndex = lastIndex + 1
       indexes.set(colHeading, newIndex)
@@ -50,4 +87,19 @@ function objectToCSV(object: Record<string, Record<string, unknown>>) {
 
   const dataAsCSV = dataAs2DArray.map((row) => row.join(",")).join("\n")
   return dataAsCSV
+}
+
+function toWeekday(dayIndex: number) {
+  const days: Record<number, string> = {
+    1: "Mon",
+    2: "Tue",
+    3: "Wed",
+    4: "Thu",
+    5: "Fri",
+    6: "Sat",
+    0: "Sun",
+  }
+
+  if (dayIndex in days) return days[dayIndex]
+  throw new Error(`${dayIndex} is not a valid weekday index`)
 }
