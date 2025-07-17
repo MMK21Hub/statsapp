4c275b86e8453a2dcb9826ce8d97d92366ba2922
MMK21Hub <50421330+MMK21Hub@users.noreply.github.com>
2025-07-17 20:56:28 +0100

    Remove directory mode from code

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,14 @@
-import { readFile, writeFile, readdir, stat } from "node:fs/promises"
+import { readFile, writeFile } from "node:fs/promises"
 import arg from "arg"
 import { Message, StatsAppConfig } from "./types.js"
 import { objectToCSV, arrayWrap, debug } from "./util.js"
 import * as path from "node:path"
-import { MergerGap, MergerPart, mergeExports } from "./merger.js"
 import { StatisticsGenerator } from "./statistics.js"
 import { messagesToChatLog, parseChatExport } from "./parser.js"
 
 export const args = arg(
   {
     "--input": String,
-    "--input-dir": String,
     "--config": String,
     "--verbose": Boolean,
     "--daily-stats": String,
@@ -37,71 +35,15 @@ async function getConfig(): Promise<StatsAppConfig> {
 
 const config = await getConfig()
 
-async function readFilesFromDirectory(
-  directory: string
-): Promise<Map<string, string>> {
-  const filenames = await readdir(directory)
-  const resultMap = new Map<string, string>()
-  const promises = filenames.map(
-    async (filename): Promise<[string, string] | undefined> => {
-      const fullPath = path.join(directory, filename)
-      const fileInfo = await stat(fullPath)
-      if (!fileInfo.isFile()) return
-      const contents = await readFile(fullPath, "utf8")
-      resultMap.set(filename, contents)
-    }
-  )
-  await Promise.all(promises)
-  return resultMap
-}
-
-function printMergeResultSummary(parts: MergerPart[], gaps: MergerGap[]) {
-  console.log(`Merged messages from ${parts.length} chat export(s):`)
-  parts.forEach(({ file, from, to }) => {
-    console.log(
-      `  ${file}: ${from.toLocaleDateString()} --> ${to.toLocaleDateString()}`
-    )
-  })
-  if (gaps.length === 0) {
-    console.log(`No gaps in the data!`)
-    return
-  }
-  console.log(`Gaps in the data:`)
-  gaps.forEach(({ from, to }) => {
-    console.log(
-      `  Missing ${from.toLocaleDateString()} --> ${to.toLocaleDateString()}`
-    )
-  })
-}
-
 async function getProcessedChatLog(): Promise<Message[]> {
   const inputFile = args["--input"]
-  const inputDir = args["--input-dir"]
-  if (inputFile) {
-    const text = await readFile(inputFile, "utf8")
-    return parseChatExport(text)
-  }
-  if (inputDir) {
-    const exports = await readFilesFromDirectory(inputDir)
-    const parsedExports: [string, Message[]][] = Array.from(exports.entries())
-      .map(([filename, text]): [string, Message[]] => [
-        filename,
-        parseChatExport(text),
-      ])
-      .sort(([_a, messages_a], [_b, messages_b]) => {
-        // Sort by the time of the first message in each export
-        const timestamp_a = messages_a[0].timestamp
-        const timestamp_b = messages_b[0].timestamp
-        return timestamp_a.valueOf() - timestamp_b.valueOf()
-      })
-    const mergeResult = mergeExports(new Map(parsedExports))
-    printMergeResultSummary(mergeResult.parts, mergeResult.gaps)
-    return mergeResult.messages
-  }
+  if (!inputFile)
+    throw new Error(
+      "You must provide the path to a chat export file, or a directory of chat export files"
+    )
 
-  throw new Error(
-    "You must provide the path to a chat export file, or a directory of chat export files"
-  )
+  const text = await readFile(inputFile, "utf8")
+  return parseChatExport(text)
 }
 
 const messages = await getProcessedChatLog()
